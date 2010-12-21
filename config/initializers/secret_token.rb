61ce247f9b061111b1418fcc812ad626d18cad81
Tim Stephenson <tim@raddonline.com>
2010-12-21 12:06:26 -0800

    Created initial rails project using Refiner CMS.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Refinery::Application.config.secret_token = '698b1170e75bb9a9ad705621ee6addf5e1a63602dbabed11cb2b3b52e57dfa4ae84639634ee085b61f45748623910f2719cbb50d451a963e279754695db09546'
